8c4caf81b50b48ca0ede41cd1a4c341776c301bb
Nodoka4318 <78198198+Nodoka4318@users.noreply.github.com>
2022-03-14 23:42:02 +0900

    新イージングの枠組みだけ

---
+++ EasyOjima/Video/Easing.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyOjima.Video {
+    public class Easing {
+        public int startFrame;
+        public int endFrame;
+        public int easeRate;
+
+        public void Ease(ref Video video) {
+            if (easeRate == 0)
+                return;
+            else {
+                if (startFrame >= endFrame || endFrame >= video.FrameSize)
+                    throw new Exception("与えられたフレーム番号が不正です。");
+                if (easeRate > 0)
+                    this.EaseIn(ref video);
+                else
+                    this.EaseOut(ref video);
+            }
+        }
+
+        public void EaseIn(ref Video video) {
+
+        }
+
+        public void EaseOut(ref Video video) {
+
+        }
+    }
+}
